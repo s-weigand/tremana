b2ab22fd9b1c362a688bebc4e7dd68e24d888c1f
s-weigand <s.weigand.phy@gmail.com>
2021-01-30 03:08:45 +0100

    Implemented custom errors

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+from textwrap import dedent
+
 import pytest
 
 from tremana.exceptions import TremanaException
+from tremana.exceptions import TremanaParsingException
+from tremana.exceptions import TremanaParsingSampleRateException
 
 
 def test_TremanaException():
@@ -8,4 +13,50 @@ def test_TremanaException():
         TremanaException,
         match=r"Foo\n\nIf you encounter a bug please open an issue at https://git\.io/JtCN6\.",
     ):
-        raise TremanaException("Foo")
+        raise TremanaException(msg="Foo")
+
+
+def test_TremanaParsingException():
+    with pytest.raises(
+        TremanaParsingException,
+        match=dedent(
+            """\
+            Foo
+
+            If you encounter a bug please open an issue at https://git\\.io/JtCN6\\."""
+        ),
+    ):
+        raise TremanaParsingException(msg="Foo")
+
+
+@pytest.mark.parametrize("origin_file_factory", ((str, Path)))
+def test_TremanaParsingException_origin_file(origin_file_factory):
+    with pytest.raises(
+        TremanaParsingException,
+        match=dedent(
+            """\
+            Foo
+
+            This Error was caused processing:
+                foo\\.txt
+
+            If you encounter a bug please open an issue at https://git\\.io/JtCN6\\."""
+        ),
+    ):
+        raise TremanaParsingException(msg="Foo", origin_file=origin_file_factory("foo.txt"))
+
+
+def test_TremanaParsingSampleRateException():
+    with pytest.raises(
+        TremanaParsingSampleRateException,
+        match=dedent(
+            """\
+            Sample rate of value '1\\.2abc' can't be cast to float, which is needed for fft\\.
+
+            This Error was caused processing:
+                foo\\.txt
+
+            If you encounter a bug please open an issue at https://git\\.io/JtCN6\\."""
+        ),
+    ):
+        raise TremanaParsingSampleRateException(sample_rate="1.2abc", origin_file="foo.txt")
--- tremana/exceptions.py
+++ tremana/exceptions.py
@@ -1,4 +1,6 @@
 """Custom exceptions for tremana."""
+import os
+from typing import Union
 
 from tremana import __repo_short_url__
 
@@ -6,6 +8,67 @@ from tremana import __repo_short_url__
 class TremanaException(Exception):
     """BaseException for expected possible errors with tremana."""
 
-    def __init__(self, msg: str = "", *args: object) -> None:  # noqa: D107
+    def __init__(self, *args: object, msg: str) -> None:  # noqa: D205, D400
+        """
+        Parameters
+        ----------
+        msg : str
+            Message to be printed.
+
+
+        .. # noqa: DAR101
+        """
         append_msg = f"If you encounter a bug please open an issue at {__repo_short_url__}."
         super().__init__(f"{msg}\n\n{append_msg}", *args)
+
+
+class TremanaParsingException(TremanaException):
+    """Baseclass for parsing Exceptions."""
+
+    def __init__(
+        self,
+        *args: object,
+        msg: str,
+        origin_file: Union[str, os.PathLike] = None,
+    ) -> None:  # noqa: D205, D400
+        """
+        Parameters
+        ----------
+        msg : str
+            Message to be printed.
+        origin_file : Union[str, os.PathLike]
+            Path to the file causing the warning, by default None
+
+
+        .. # noqa: DAR101
+        """
+        if origin_file:
+            msg = f"{msg}\n\nThis Error was caused processing:\n    {str(origin_file)}"
+        super().__init__(*args, msg=msg)
+
+
+class TremanaParsingSampleRateException(TremanaParsingException):
+    """Error thrown when the samplerate can't be parsed to a numerical value."""
+
+    def __init__(
+        self,
+        *args: object,
+        sample_rate: str,
+        origin_file: Union[str, os.PathLike] = None,
+    ) -> None:  # noqa: D205, D400
+        """
+        Parameters
+        ----------
+        sample_rate : str
+            Value of the sample rate which should have been cast to float.
+        origin_file : Union[str, os.PathLike]
+            Path to the file causing the warning, by default None
+
+
+        .. # noqa: DAR101
+        """
+        msg = (
+            f"Sample rate of value {sample_rate!r} "
+            "can't be cast to float, which is needed for fft."
+        )
+        super().__init__(*args, msg=msg, origin_file=origin_file)
